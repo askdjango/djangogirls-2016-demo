892afbb57ec25330cdbe9be83f632126229b09f7
Chinseok Lee <allieuslee@gmail.com>
2016-06-06 14:42:10 +0900

    blog.Post 모델 생성하고, 마이그레이션 적용

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    # author = models.ForeignKey('auth.User')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title    
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
