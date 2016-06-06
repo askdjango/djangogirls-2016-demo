17a2135b9e12b5e5fa36da3d0763415b03c1ef96
Chinseok Lee <allieuslee@gmail.com>
2016-06-06 15:38:25 +0900

    blog post_new/post_edit 뷰 구현

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Post
+from .forms import PostForm
 
 
 # lte : less than equal     <=
@@ -21,4 +23,34 @@ def post_detail(request, pk):
 
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-    
+
+
+@login_required(login_url='admin:login')
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required(login_url='admin:login')
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
