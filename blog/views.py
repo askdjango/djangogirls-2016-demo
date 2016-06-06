b2c2cac0fea8d049d783321f166170139b483f05
Chinseok Lee <allieuslee@gmail.com>
2016-06-06 15:21:02 +0900

    blog.post_detail 뷰 구현

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 
@@ -11,4 +11,14 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+    
+
+def post_detail(request, pk):
+#   try:
+#       post = Post.objects.get(pk=pk)  # Post.DoesNotExist
+#   except Post.DoesNotExist:
+#       raise Http404  # django.http.Http404
+
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
     
